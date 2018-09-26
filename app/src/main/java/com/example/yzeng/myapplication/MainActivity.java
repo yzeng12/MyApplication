cf22a8a65cfdbc7cb50f1546c57e126df7c55de0
yzeng12 <zengyx1995@gmail.com>
2018-09-26 17:02:49 -0500

    first commit

---
+++ app/src/main/java/com/example/yzeng/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.yzeng.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        int i;
+        TextView MyFirstName=findViewById(R.id.MyFirstName);
+        MyFirstName.setText("Yixin Zeng");
+
+    }
+    private void  TestUpLoad(){
+        int i= 1+1;
+    }
+}

3ad7309804eabc3e503e79f157f48e16c1f5eae8
yzeng12 <zengyx1995@gmail.com>
2018-09-27 09:10:05 -0500

    second commit

--- app/src/main/java/com/example/yzeng/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yzeng/myapplication/MainActivity.java
@@ -11,12 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
-        int i;
-        TextView MyFirstName=findViewById(R.id.MyFirstName);
-        MyFirstName.setText("Yixin Zeng");
+//        int i;
+//        TextView MyFirstName=findViewById(R.id.MyFirstName);
+//        MyFirstName.setText("Yixin Zeng");
 
     }
-    private void whatever(){
-        int i=1+1;
-    }
+
 }
